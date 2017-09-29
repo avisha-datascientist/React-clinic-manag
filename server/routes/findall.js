23a7c43552ddde9b598fe4ce301849c0f0e46b03
avisha97 <31319243+avisha97@users.noreply.github.com>
2017-09-29 05:59:33 +0530

    finished auth flow

---
+++ server/routes/findall.js
@@ -0,0 +1,7 @@
+module.exports = function(app){
+    app.get('/find/all',function(req,res){
+
+
+    })
+
+}
---
+++ server/services/passport.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const mongoose = require('mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser(function(user,done){
+done(null,user.id);
+
+});
+
+passport.deserializeUser(function(id,done){
+User.findById(id).then(function(user){
+    done(null,user);
+})
+
+});
+
+
+passport.use(new GoogleStrategy({
+        clientID:keys.googleClientID,
+        clientSecret:keys.googleClientSecret,
+        callbackURL:'/auth/google/callback'
+    },function(accessToken, refreshToken, profile, done){
+User.findOne({googleId: profile.id})
+    .then(function(existingUser){
+            if(existingUser)
+            { done(null,existingUser); }
+        else
+            {
+                new User({googleId: profile.id}).save()
+                    .then(function(user){
+                        done(null,user);
+                    })
+            }
+
+        })
+
+    })
+);
